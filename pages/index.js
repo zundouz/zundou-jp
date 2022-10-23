7cd0e2001b8400f7ee115fb838bdcee6eed28508
k-tsuda <h240503@gmail.com>
2022-10-23 19:15:41 +0900

    feat: 最低限の情報を載せた

--- pages/index.js
+++ pages/index.js
@@ -13,15 +13,56 @@ export default function Home() {
         <h1 className={styles.title}>
           zundou
         </h1>
-
-        <p className={styles.description}>
-          制作物リンク
+          <h2>event log</h2>
           <ul>
-          <li>A</li>
-          <li>B</li>
-          <li>C</li>
+            <li>unity1week 参加作品</li>
+            <a href="https://unityroom.com/games/borobaketsu_bakkun">
+              <img 
+                src="/u1w_baketsu.gif"
+                alt="u1w_baketsu"
+                width="40%"
+                height="40%"
+              />
+            </a>
+            <li>Twitterの時間制限付き制作イベント</li>
+              <ul>
+                <li>
+                  <a href="https://twitter.com/search?q=%23%E6%B7%B1%E5%A4%9C%E3%81%AE2%E6%99%82%E9%96%93DTM%20from%3A%40zundou_&src=typed_query&f=live">
+                    「#深夜の2時間DTM」 参加ログ
+                  </a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/search?q=%2310%E5%88%86%E3%83%89%E3%83%83%E3%83%88%20from%3A%40zundou_&src=typed_query&f=live">
+                    「#10分ドット」 参加ログ
+                  </a>
+                </li>
+              </ul>
           </ul>
-        </p>
+          <h2>other link</h2>
+            <a href="https://unityroom.com/users/mlcw42qtxj1hvk5upa3z">
+              <img 
+                src="/urcon.png"
+                alt="unityroom_link"
+                width="10%"
+                height="10%"
+              />
+            </a>
+            <a href="https://soundcloud.com/zundou">
+              <img 
+                src="/scicon.png"
+                alt="soundcloud_link"
+                width="10%"
+                height="10%"
+              />
+            </a>
+            <a href="https://twitter.com/zundou_">
+              <img 
+                src="/twicon.png"
+                alt="twitter_link"
+                width="10%"
+                height="10%"
+              />
+            </a>
       </main>
 
       <footer className={styles.footer}>
